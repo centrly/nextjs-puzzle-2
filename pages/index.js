e11295a1190918150090ef20ec0d527cd37f6460
Ted Nichols <foster.nichols@gmail.com>
2022-09-23 09:01:27 -0400

    edit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 const Index = () => (
   <div style={{fontFamily: "courier, courier new, serif;"}}>
     <p>Congratulations! You solved Puzzle 3 of the MIT 2022 Career Fair Puzzle Hunt.</p>
-    <p>Email - <a href="katia.paramonova@centrly.com">katia.paramonova@centrly.com</a> with the word “Kermit”! Do it ASAP as your ranking depends on it :D</p>
+    <p>Email - <a href="katia.paramonova@centrly.com">katia.paramonova@centrly.com</a> with the subject line “Kermit”! Do it ASAP as your ranking depends on it :D</p>
   </div>
 )
 export default Index;
