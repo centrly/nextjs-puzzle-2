65a44b0d3dd6dd66ac5a5c6b96856d611908b0c8
Ted Nichols <foster.nichols@gmail.com>
2022-09-23 00:00:01 -0400

    add coordinates

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 const Index = () => (
-  <div>
-    Hi Katia, puzzle 3 content goes here.{' '}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
+  <div style={{fontFamily: "courier, courier new, serif;"}}>
+    <p>42.35716336173117, -71.09524707429696 S 1</p>
+    <p>42.360304758949596, -71.09481933294093 NW 9</p>
+    <p>42.36105399126457, -71.09656388861532 SW 5</p>
+    <p>42.359108708311304, -71.09339452371319 NE 1 14 17</p>
+    <p>Answer: _ _ _ _ _ _</p>
+    <p>Hints: Always read top-down. And to IRL or not to IRL, that's the Q.</p>
   </div>
 )
 export default Index;
