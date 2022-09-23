f8844035d25670397072314e6835855ae85d052e
Ted Nichols <foster.nichols@gmail.com>
2022-09-23 09:11:05 -0400

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
 const Index = () => (
   <div style={{fontFamily:"courier, courier new, serif;"}}>
+    <p><ital>You came, you saw, you ciphered.</ital></p>
     <p>elvjc pfljf despa bapsl cscpf bblkK fksak yldcj aqlpc qxqsa jlbri cqqfb frcsf lk</p>
     <p>Answer: _ _ _ _ _ _ _ _</p>
+    <p>Hint: You already have the key.</p>
+    <p>As expected, go to the following to unlock Puzzle 2: ????????.centrly.com</p>
+    <p>Reach out to contact@centrly.com if you need help.</p>
   </div>
 )
 export default Index;
