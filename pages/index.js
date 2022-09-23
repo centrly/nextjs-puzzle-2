eae225e7baf1546ae5387a4efc5595059341b4e3
Ted Nichols <foster.nichols@gmail.com>
2022-09-23 00:10:07 -0400

    final message

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Link from 'next/link'
 const Index = () => (
   <div style={{fontFamily: "courier, courier new, serif;"}}>
-    <p>42.35716336173117, -71.09524707429696 S 1</p>
-    <p>42.360304758949596, -71.09481933294093 NW 9</p>
-    <p>42.36105399126457, -71.09656388861532 SW 5</p>
-    <p>42.359108708311304, -71.09339452371319 NE 1 14 17</p>
-    <p>Answer: _ _ _ _ _ _</p>
-    <p>Hints: Always read top-down. And to IRL or not to IRL, that's the Q.</p>
+    <p>Congratulations! You solved Puzzle 3 of the MIT 2022 Career Fair Puzzle Hunt.</p>
+    <p>Email - <a href="katia.paramonova@centrly.com">katia.paramonova@centrly.com</a> with the word “Kermit”! Do it ASAP as your ranking depends on it :D</p>
   </div>
 )
 export default Index;
