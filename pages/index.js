826b3be0cbf995ab91bc87f92b2df108ae6675ff
Ted Nichols <foster.nichols@gmail.com>
2022-09-23 09:12:32 -0400

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 const Index = () => (
   <div style={{fontFamily: "courier, courier new, serif;"}}>
+    <p>Congrats, you successfully solved Puzzle 2! Email - <a href="mailto:katia.paramonova@centrly.com">katia.paramonova@centrly.com</a> with the subject line “Taxonomy”! Do it ASAP as your ranking for Puzzle 2 depends on it :D</p>
+    <br/>
     <p>42.35716336173117, -71.09524707429696 S 1</p>
     <p>42.360304758949596, -71.09481933294093 NW 9</p>
     <p>42.36105399126457, -71.09656388861532 SW 5</p>
     <p>42.359108708311304, -71.09339452371319 NE 1 14 17</p>
     <p>Answer: _ _ _ _ _ _</p>
     <p>Hints: Always read top-down. And to IRL or not to IRL, that's the Q.</p>
+    <p>As always, enter in for the finale: ??????.centrly.com</p>
   </div>
 )
 export default Index;
